73c6b44fd015f0f346e27bae2993707e8d672b10
ppmasa8 <kiyokiyopip56@gmail.com>
2021-04-10 21:31:53 +0900

    主要機能実装途中

---
+++ app/messages/message_definission.rb
@@ -0,0 +1,60 @@
+FIRST_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+SECOND_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+THIRD_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+FOURTH_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+FIFTH_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+SIXTH_ANSWER = {
+  1 => "",
+  2 => "",
+  3 => "",
+  4 => ""
+}.freeze
+ANSWER = {
+  10 => FIRST_ANSWER,
+  20 => SECOND_ANSWER,
+  30 => THIRD_ANSWER,
+  40 => FOURTH_ANSWER,
+  50 => FIFTH_ANSWER,
+  60 => SIXTH_ANSWER
+}.freeze
+
+
+FIRST_QUESTION  = ""
+SECOND_QUESTION = ""
+THIRD_QUESTION  = ""
+FOURTH_QUESTION = ""
+FIFTH_QUESTION  = ""
+SIXTH_QUESTION  = ""
+QUESTION = {
+  1 => FIRST_QUESTION,
+  2 => SECOND_QUESTION,
+  3 => THIRD_QUESTION,
+  4 => FOURTH_QUESTION,
+  5 => FIFTH_QUESTION,
+  6 => SIXTH_QUESTION
+}.freeze
